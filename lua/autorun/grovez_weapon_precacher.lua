40dd3100e48733deee84b8d0301d6e2ce7bbe94e
GroveZ <75123394+GroveZ45@users.noreply.github.com>
2022-06-06 23:07:39 +0700

    All viewmodels should be precached now

---
+++ lua/autorun/grovez_weapon_precacher.lua
@@ -0,0 +1,8 @@
+hook.Add("InitPostEntity", "grovez_weapon_precacher", function()
+    for _, SWEP in ipairs(weapons.GetList()) do
+        if SWEP.ClassName and SWEP.ClassName:StartWith("tfa_grovez_") and weapons.IsBasedOn(SWEP.ClassName, "tfa_custom_base") then
+            util.PrecacheModel(SWEP.ViewModel)
+            print("Precached: " .. SWEP.ViewModel)
+        end
+    end
+end)
