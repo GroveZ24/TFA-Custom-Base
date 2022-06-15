329847dd8c7b5a004f0be36fdb4d48fc36b32776
GroveZ <75123394+GroveZ45@users.noreply.github.com>
2022-06-16 03:30:27 +0700

    Revert "Disallow movement in customization, bootleg method"
    This reverts commit 4f81df789f6798c155743612f5e31118d3528cd2.

--- lua/weapons/tfa_custom_base/shared.lua
+++ lua/weapons/tfa_custom_base/shared.lua
@@ -4,20 +4,6 @@ DEFINE_BASECLASS("tfa_gun_base")
 
 SWEP.HasFlashlight = false
 
-----[[DISALLOW MOVEMENT IN CUSTOMIZATION]]----
-
-if CLIENT then
-	hook.Add("StartCommand", "TFA_DisallowMovement", function(ply, ucmd)
-		if ply:GetActiveWeapon():GetInspectingProgress() > 0.01 then
-			RunConsoleCommand("-forward")
-			RunConsoleCommand("-back")
-			RunConsoleCommand("-moveright")
-			RunConsoleCommand("-moveleft")
-			ucmd:RemoveKey(IN_JUMP)
-		end
-	end)
-end
-
 ----[[DRAW SINGLE RETICLE]]----
 
 function DrawSingleReticle()
