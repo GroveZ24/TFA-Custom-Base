0cdc52c5089ed3114481824e8ae77f2086bb96cd
GroveZ <75123394+GroveZ45@users.noreply.github.com>
2022-07-14 17:52:39 +0700

    Stalol's matproxies

---
+++ lua/autorun/stalol_matproxy.lua
@@ -0,0 +1,74 @@
+local basevec = Vector(1, 1, 1)
+local resvar = "$envmaptint"
+
+matproxy.Add({
+    name = "SAA_CubemapLightMult",
+
+    init = function(self, mat, values)
+        self.ResultVar = values.resultvar or resvar
+        self.MultVar = values.multiplier
+    end,
+
+    bind = function(self, mat, ent)
+        local rgbvec = basevec
+
+        if IsValid(ent) then
+            local mv = self.MultVar
+            local lightvec = render.GetLightColor(ent:GetPos())
+            local average = (lightvec[1] + lightvec[2] + lightvec[3]) / 3
+            local coeff = mv and mat:GetVector(mv) or basevec
+            rgbvec = Lerp(RealFrameTime() * 10, mat:GetVector(self.ResultVar), coeff * average)
+        end
+
+        mat:SetVector(self.ResultVar, rgbvec)
+    end
+})
+
+matproxy.Add({
+    name = "SAA_CubemapTintMult",
+
+    init = function(self, mat, values)
+        self.ResultVar = values.resultvar or resvar
+        self.MultVar = values.multiplier
+        self.Min = values.min
+        self.Max = values.max
+    end,
+
+    bind = function(self, mat, ent)
+        local rgbvec = basevec
+
+        if IsValid(ent) then
+            local mv = self.MultVar
+            local lightvec = render.GetLightColor(ent:GetPos())
+            local coeff = mv and mat:GetVector(mv) or basevec
+            rgbvec = Lerp(RealFrameTime() * 10, mat:GetVector(self.ResultVar), coeff * lightvec)
+        end
+
+        mat:SetVector(self.ResultVar, rgbvec)
+    end
+})
+
+local exvar = "$color2"
+
+matproxy.Add({
+    name = "SAA_ColorLightMult",
+
+    init = function(self, mat, values)
+        self.ResultVar = values.resultvar or exvar
+        self.MultVar = values.multiplier
+    end,
+
+    bind = function(self, mat, ent)
+        local rgbvec = basevec
+
+        if IsValid(ent) then
+            local mv = self.MultVar
+            local lightvec = render.GetLightColor(ent:GetPos())
+            local average = (lightvec[1] + lightvec[2] + lightvec[3]) / 3
+            local coeff = mv and mat:GetVector(mv) or basevec
+            rgbvec = Lerp(RealFrameTime() * 10, mat:GetVector(self.ResultVar), coeff * average)
+        end
+
+        mat:SetVector(self.ResultVar, rgbvec)
+    end
+})
